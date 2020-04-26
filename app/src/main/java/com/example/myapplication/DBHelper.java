c8a186acef681a29c848a493c6a98540b85f1d4a
cshevlin <Dogbarkwalk2*>
2020-04-26 10:22:48 +0100

    Rolled back DB changes in favour of official Android docs process

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -1,47 +1,27 @@
 package com.example.myapplication;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
+import com.example.myapplication.DBContract;
 
 public class DBHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "app_database";
-    public static final String USER_TABLE_NAME = "user";
-    public static final String USER_COLUMN_ID = "_id";
-    public static final String USER_COLUMN_NAME = "userID";
-    public static final String USER_COLUMN_CLICKTOTAL = "clickTotal";
+    DBContract db = new DBContract();
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "ButtonDB.db";
 
-    public DBHelper(Context context) {
+    public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE " + USER_TABLE_NAME + " (" +
-                USER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                USER_COLUMN_NAME + " TEXT, " +
-                USER_COLUMN_CLICKTOTAL + " INTEGER" + ")");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTERIES);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + USER_TABLE_NAME);
-        onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
     }
-
-    private void saveToDB() {
-        SQLiteDatabase database = new DBHelper(this).getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(DBHelper.USER_COLUMN_NAME, activityBinding.nameEditText.getText().toString());
-        values.put(DBHelper.USER_COLUMN_AGE, activityBinding.ageEditText.getText().toString());
-        values.put(DBHelper.USER_COLUMN_GENDER, activityBinding.genderEditText.getText().toString());
-        long newRowId = database.insert(SampleSQLiteDBHelper.PERSON_TABLE_NAME, null, values);
-
-        Toast.makeText(this, "The new Row Id is " + newRowId, Toast.LENGTH_LONG).show();
-    }
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     int buttonCounter;
+
+    // Set up, draw button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +29,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
