0b1d5f6c9403730667a269029b280cbea56d2355
cshevlin <Dogbarkwalk2*>
2020-06-23 23:11:07 +0100

    Updated GET requests to better parse JSONArray response and avoid JSONParseErrror

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,6 +20,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.myapplication.HTTPAccess;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
@@ -27,10 +28,16 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
     HTTPAccess access = new HTTPAccess();
     int buttonCounter;
+    /*
+    TODO
+    Establish schema and functionality for users
+     */
     String user = "Test User";
+    String responseValue = "";
     private DBHelper db;
     private long currentId;
-    // Set up, draw buttons
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,34 +65,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //final Button sendToServerButton = findViewById(R.id.sendToServerButton_id);
-        //sendToServerButton.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-              //  sendToServer();
-            //}
-        //});
 
+        final Button sendToServerButton = findViewById(R.id.sendToServerButton_id);
+        sendToServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToServer();
+                textViewServerReponse.setText(sendToServer());
+            }
+        });
+    }
+
+    /*
+    TODO
+    Break this out service out into its own class
+     */
+    private String sendToServer() {
         RequestQueue queue = Volley.newRequestQueue(this);
         String URL = access.getAccessToken();
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
-                new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONArray response) {
                         Log.d("Response from server", "onResponse: " + response.toString());
-                        textViewServerReponse.setText(response.toString());
+                        responseValue = response.toString();
                     }
                 }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d("Server response", "onErrorResponse: " + " Server response error" + error);
-                textViewServerReponse.setText(error.toString());
+                responseValue = error.toString();
             }
 
         });
         queue.add(jsonRequest);
+        return responseValue;
     }
+
 
         private void writeToDB(){
         String currentTime = Calendar.getInstance().getTime().toString();
@@ -97,8 +114,6 @@ public class MainActivity extends AppCompatActivity {
             final TextView textViewResult = findViewById(R.id.textViewResult_id);
             textViewResult.setText("Number of times pressed: " + db.getMostRecent(id) );
     }
-
-
 
 
 }
